e6b7439f3c9c8dda65420430ef7da24021cef047
Oren Recht <oren.recht@riskfirst.com>
2020-09-09 18:28:57 +0100

    Get shabbat times from hebcal REST API

--- AddShabbatTimesToCalendar.ps1
+++ AddShabbatTimesToCalendar.ps1
@@ -1,29 +1,27 @@
 import-module .\OutlookTools.psm1
 
-$shabbatTimes = "5 Jan 2018 	3:50pm
-12 Jan 2018 	4:01pm
-19 Jan 2018 	4:12pm
-26 Jan 2018 	4:24pm
-2 Feb 2018 	4:36pm 
-9 Feb 2018 	4:49pm
-16 Feb 2018 	5:02pm
-23 Feb 2018 	5:15pm
-2 Mar 2018 	5:28pm
-9 Mar 2018 	5:40pm
-16 Mar 2018 	5:52pm
-23 Mar 2018 	6:04pm" -Split "\n"
-
+# Config
+$city = 'GB-London'
 $minutesRequiredBeforeShabbat = 120
+$year = 2020
+$months = 9, 10, 11, 12
 
-foreach ($shabbatStartTimeString in $shabbatTimes)
+# Code
+foreach ($month in $months)
 {
-    #$shabbatStartTime = [datetime]::Parse("5 Jan 2018 3:50pm")
-    $shabbatStartTime = [datetime]::Parse($shabbatStartTimeString)
-    $timeRequiredBeforeShabbat = [timespan]::FromMinutes($minutesRequiredBeforeShabbat)
+    $url = "https://www.hebcal.com/hebcal?v=1&cfg=json&c=on&m=0&year=$year&month=$month&city=$city"
 
-    $meetinStart = $shabbatStartTime - $timeRequiredBeforeShabbat
+    $shabbatTimes = ((curl $url).Content | ConvertFrom-Json).items | select-object  date 
 
-    echo "Create meeting from ${meetinStart} for duration of 240 min"
-    New-OutlookCalendarMeeting -Subject Shabbat -BusyStatus 3 -Body " " -Location " " -MeetingStart $meetinStart -MeetingDuration 240
-    
+    foreach ($ShabbatStartTimeObj in $shabbatTimes)
+    {
+        $shabbatStartTime = [datetime]::Parse($ShabbatStartTimeObj.date)
+        $timeRequiredBeforeShabbat = [timespan]::FromMinutes($minutesRequiredBeforeShabbat)
+
+        $meetinStart = $shabbatStartTime - $timeRequiredBeforeShabbat
+
+        echo "Create meeting from ${meetinStart} for duration of 240 min"
+        New-OutlookCalendarMeeting -Subject Shabbat -BusyStatus 3 -Body " " -Location " " -MeetingStart $meetinStart -MeetingDuration 240
+        
+    }
 }
